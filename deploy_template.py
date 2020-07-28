0d97df415d9509bb1334bb2bdbe6e73dbc3aa83a
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-07-28 10:12:46 +0600

    Task deployment script

---
+++ deploy_template.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shutil
+import subprocess
+from github import Github
+
+
+def git(cmds):
+    print("git {}".format(" ".join(cmds)))
+    subprocess.run(["git"] + cmds, check=True)
+
+name = sys.argv[1]
+
+shutil.rmtree(".git")
+
+git(["init"])
+git(["add", "--all"])
+git(["rm", "--cached", ".gitlab-ci.yml", "deploy_template.py"])
+git(["commit", "-m", "Deploy task template"])
+git(["remote", "add", "origin", "git@github.com:NSU-Programming/{}.git".format(name)])
+
+print("accessing Github account")
+with open(os.path.expanduser("~/.github/token")) as f:
+    gt = f.read().strip("\n")
+g = Github(gt)
+org = g.get_organization("NSU-Programming")
+repos = [repo.name for repo in org.get_repos()]
+if name not in repos:
+    print("creating repo '{}'".format(name))
+    org.create_repo(name, private=True)
+
+git(["push", "-f", "origin", "master"])
